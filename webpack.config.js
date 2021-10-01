7391102dc75c04b8620d96f698c2f064045adbe2
Matthieu Bessat <spamfree@matthieubessat.fr>
2021-10-01 18:46:44 +0200

    refactor: separation of scss
    refactor: variables names
    fix: add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: [
+    './src/main.js',
+    './assets/favicon.png'
+  ],
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
@@ -10,6 +13,17 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: './'
+            }
+          }
+        ]
+      },
       {
         test: /\.css$/,
         use: [
